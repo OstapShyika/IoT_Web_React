e7549c070b672d69190d1f9442296e49051b3334
OstapShyika <92269764+OstapShyika@users.noreply.github.com>
2022-10-26 09:55:51 +0300

    Added lab 6 code

---
+++ src/components/Item/item.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './item.css'
+import mov from './Dark-Knight-70822-scaled-768x1137.jpg'
+
+export default function Item() {
+  return (
+    <div className='item'>
+      <img src={mov} alt="movie poster" className='item-img'/>
+      <h1>The Dark Night</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+    </div>
+  )
+}
